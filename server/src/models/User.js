c3681837388b6a34a22fa4ef62245274f220728a
Desirée Strand <desireestrxnd@gmail.com>
2026-05-12 13:38:03 +0200

    add user model with password hashing

---
+++ server/src/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    hemisphere: {
+      type: String,
+      required: true,
+      enum: ["northern", "southern"],
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+export default mongoose.model("User", userSchema);
